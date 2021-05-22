8b1b2561be69d87e43c8e4e9f204750e6a486556
jaimepastor <jjaimepastor@gmail.com>
2021-05-23 00:21:29 +0800

    initial commit

---
+++ static/js/index.js
@@ -0,0 +1,20 @@
+var width = 800;
+var height = 800;
+
+var chart1 = d3.select("#chart1").append("svg")
+                .attr("width", width)
+                .attr("height", height)
+
+var chart2 = d3.select("#chart2").append("svg")
+                .attr("width", width)
+                .attr("height", height)
+
+var chart3 = d3.select("#chart3").append("svg")
+                .attr("width", width)
+                .attr("height", height)
+
+// chart ng History
+
+// chart ng Trade Linkages
+// chart ng Top Commodities (Country)
+// chart ng Top Commodities (Commodities)
